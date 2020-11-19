b194207564a11c7749b4910658f0eba859d09e29
LINOS NCHENA <linosnchena@gmail.com>
2020-11-19 19:34:11 +0100

    first commit

---
+++ barclays/zanacoAPI/Models/DataBaseContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace zanacoAPI.Models
+{
+   
+
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Student> Student { get; set; }
+
+        internal void SaveChanges1()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+}
---
+++ barclays/zanacoAPI/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace zanacoAPI.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Rollno { get; set; }
+        public string Class { get; set; }
+        public string Address { get; set; }
+        public int id { get; internal set; }
+    }
+}
